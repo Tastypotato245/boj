03a0f445d3724433347228e3c8111bc2eaede034
kyusulee <rbt2775@cau.ac.kr>
2024-02-12 14:53:36 +0900

    add: 11444

--- 11444/main.cpp
+++ 11444/main.cpp
@@ -1,47 +1,31 @@
-// https://www.acmicpc.net/problem/
+// https://www.acmicpc.net/problem/11444
 
 #include <iostream>
-#include <algorithm>
-
+#include <map>
 using namespace std;
 
-int	N = 0;
-vector<long long> PiboVec;
+map<long,long> m;
 
-void Input()
-{
-	cin >> N;
+#define MOD 1000000007
+
+long long fib(long long x){
+    long long result;
+
+    if(m[x]) return m[x];
+    if(x % 2 == 0)
+		result = (fib(x/2)*(fib(x/2+1)+fib(x/2-1))) % MOD;
+    else
+		result = ((fib((x+1)/2) * fib((x+1)/2)) % MOD)+((fib((x-1)/2)*fib((x-1)/2)) % MOD) % MOD;
+    return m[x] = result % MOD;
 }
 
-void MakePiboVec()
-{
-	int	i = 1;
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
 
-	PiboVec.push_back(0);
-	PiboVec.push_back(1);
-	while (++i < N)
-		PiboVec.push_back(PiboVec[i - 1] + PiboVec[i - 2]);
-}
-
-void Solve()
-{
-	MakePiboVec();
-
-}
-
-void Output()
-{
-	cout << PiboVec[N - 1] << "\n";
-}
-
-int	main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	Input();
-	Solve();
-	Output();
-	return (0);
+    long long n;
+    cin >> n;
+    m[0] = 0;
+    m[1] = 1;
+    m[2] = 1;
+    cout << fib(n);
 }
