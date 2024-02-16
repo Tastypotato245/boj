8cff84ae9fbdedf45947cb9f60c9cad71607c02b
kyusulee <rbt2775@cau.ac.kr>
2024-02-16 15:13:22 +0900

    add: 24416

--- 24416/main.cpp
+++ 24416/main.cpp
@@ -1,4 +1,4 @@
-// https://www.acmicpc.net/problem/
+// https://www.acmicpc.net/problem/24416
 // github/Tastypotato245
 
 #include <iostream>
@@ -7,10 +7,16 @@
 using namespace std;
 
 int	N;
+long long fibo[41];
+
 
 void Solve()
 {
 	cin >> N;
+	fibo[1] = fibo[2] = 1;
+	for (int i = 3 ; i <= N ; ++i)
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	cout << fibo[N] << " " << ((N > 2) ? (N - 2) : 0) << "\n";
 }
 
 int	main(void)
