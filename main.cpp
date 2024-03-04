79f5e204a4958ce5d4a3ac6a67c9bd5b8ec25877
kyusulee <rbt2775@cau.ac.kr>
2024-03-04 18:48:23 +0900

    add: 1309

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/1541
+// github/Tastypotato245
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int	N;
+int ans = -1;
+
+void Solve()
+{
+	cin >> N;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
