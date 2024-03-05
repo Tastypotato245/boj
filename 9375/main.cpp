b45556150881b0ab65e2f63c303abf079f5c7f70
Tastypotato245 <rbt2775@cau.ac.kr>
2024-03-05 15:55:01 +0900

    add: 9375

--- 9375/main.cpp
+++ 9375/main.cpp
@@ -10,20 +10,29 @@ using namespace std;
 
 int	N;
 int	M;
-map<string, string> map;
+long long sum;
 
 void Solve()
 {
 	cin >> N;
-	for (int i < 0 ; i < N ; ++i)
+	for (int i = 0 ; i < N ; ++i)
 	{
 		cin >> M;
+		sum = 1;
+		map<string, int> m;
 		string val, key;
-		for (int j < 0 ; j < M ; ++j)
+		for (int j = 0 ; j < M ; ++j)
 		{
 			cin >> val >> key;
-			map.insert(key, val);
+			if (m.find(key) != m.end())
+				m[key] += 1;
+			else
+				m[key] = 1;
 		}
+		for (auto item : m)
+			sum *= (item.second + 1);
+		sum -= 1;
+		cout << sum << "\n";
 	}
 }
 
