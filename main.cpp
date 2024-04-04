42bdd055b70424466eee6ba48ae0c133593c4702
Tastypotato245 <rbt2775@cau.ac.kr>
2024-04-04 23:12:53 +0900

    add: 3651

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/3651
+// github/Tastypotato245
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
