ebd3442dd2d3a59e5ee4c0e6e353441b54e9d201
Tastypotato245 <rbt2775@cau.ac.kr>
2024-04-25 12:56:00 +0900

    add: test code

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/11657
+// github/Tastypotato245
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
