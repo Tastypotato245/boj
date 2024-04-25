7358374603c2e653ba793d372936a852499105a1
Tastypotato245 <rbt2775@cau.ac.kr>
2024-04-25 17:08:17 +0900

    fixing

--- 2/2458/main.cpp
+++ 2/2458/main.cpp
@@ -25,11 +25,14 @@ void Solve()
 		for (int i = 1 ; i <= N ; ++i)
 			for (int j = 1 ; j <= N ; ++j)
 				if (dis[i][j] == 0 && dis[i][k] != 0 && dis[k][j] != 0)
-				{
 					dis[i][j] = 1;
-					++dis[0][i];
-					++dis[0][j];
-				}
+	for (int i = 1 ; i <= N ; ++i)
+		for (int j = 1 ; j <= N ; ++j)
+			if (i != j && dis[i][j] != 0)
+			{
+				dis[0][i]++;
+				dis[0][j]++;
+			}
 	int cnt = 0;
 	for (int i = 1 ; i <= N; ++i)
 		if (dis[0][i] == N - 1)
