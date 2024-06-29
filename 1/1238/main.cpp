4f345a08056d29dc81ed82c03f2476d38c78c8cd
Tastypotato245 <rbt2775@cau.ac.kr>
2024-06-30 07:02:19 +0900

    update: 1238

--- 1/1238/main.cpp
+++ 1/1238/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#define INT_INF 1000000000
+#define INT_INF 1000000001
 
 using namespace std;
 
@@ -15,13 +15,23 @@ void Solve()
 	cin >> N >> M >> X;
 	int a, b;
 	for (int i = 1 ; i <= N ; ++i)
-		fill(path[i] + 1, path[i] + N + 1, INT_INF);
+		for (int j = 1 ; j <= N ; ++j)
+		{
+			if (i == j)
+				continue ;
+			path[i][j] = INT_INF;
+		}
+
 	for (int i = 1 ; i <= M ; ++i)
 		cin >> a >> b >> path[a][b];
-	for (int i = 1 ; i <= N ; ++i)
-		for (int j = 1 ; j <= N ; ++j)
-			for (int k = 1 ; k <= N ; ++k)
+
+	for (int k = 1 ; k <= N ; ++k)
+		for (int i = 1 ; i <= N ; ++i)
+			for (int j = 1 ; j <= N ; ++j)
 				path[i][j] = min(path[i][j], path[i][k] + path[k][j]);
+
+	for (int i = 1 ; i <= N ; ++i)
+		path[0][i] = path[i][X] + path[X][i];
 	cout << *max_element(path[0] + 1, path[0] + N + 1) << "\n";
 }
 
