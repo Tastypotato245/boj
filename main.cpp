36910455b0ba0a975ee3a523502ecf6f557d55d1
Tastypotato245 <rbt2775@cau.ac.kr>
2024-07-16 18:52:45 +0900

    add: 1197 not solve

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/1197
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
