4b8136eae843a14a805ce98fe03c53f47c95b639
Tastypotato245 <rbt2775@cau.ac.kr>
2024-11-15 14:57:07 +0900

    add: 9657

---
+++ 9/9657/main.cpp
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/9657
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long	N;
+
+void Solve()
+{
+	cin >> N;
+	long long c = N % 7;
+	if (c == 0 || c == 2)
+		cout << "CY" << "\n";
+	else
+		cout << "SK" << "\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
