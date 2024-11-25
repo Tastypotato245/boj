103419204ac8db5924f162335773036e89294232
Tastypotato245 <rbt2775@cau.ac.kr>
2024-11-25 09:54:57 +0900

    add: 20455

---
+++ 3/30455/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/30455
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+	cout << (N % 2 ? "Goose" : "Duck") << "\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
