daf23c4f297a217c185f4324a21f67682ad0d032
tastypotato245 <rbt2775@cau.ac.kr>
2024-11-30 10:39:01 +0900

    add: some codes

---
+++ 1/10926/main.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/10926
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+void Solve()
+{
+	string s;
+	cin >> s;
+	cout << s << "\?\?!" << "\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
