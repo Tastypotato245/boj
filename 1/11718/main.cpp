bf3799fa3d352509f442e8bbd589a1da9a0dc545
tastypotato245 <rbt2775@cau.ac.kr>
2024-12-05 15:45:14 +0900

    add: 11718

---
+++ 1/11718/main.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/11718
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+void Solve()
+{
+	string s;
+	while (getline(cin, s))
+		cout << s << "\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
