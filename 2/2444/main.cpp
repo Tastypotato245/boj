42cf8434de1c797fcabcb42fd9efbc4b1f48e33b
Tastypotato245 <rbt2775@cau.ac.kr>
2024-12-06 16:36:20 +0900

    add: 2444

---
+++ 2/2444/main.cpp
@@ -0,0 +1,38 @@
+// https://www.acmicpc.net/problem/2444
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+
+	for (int i = 0 ; i < N ; ++i)
+	{
+		for (int j = 0 ; j < N - 1 - i ; ++j)
+			cout << " ";
+		for (int j = 0 ; j < i * 2 + 1 ; ++j)
+			cout << "*";
+		cout << "\n";
+	}
+	for (int i = 1 ; i < N ; ++i)
+	{
+		for (int j = 0 ; j < i ; ++j)
+			cout << " ";
+		for (int j = 0 ; j < (N - 1 - i) * 2 + 1 ; ++j)
+			cout << "*";
+		cout << "\n";
+	}
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
