dd80216fd23726d959c92d731167e3143f6da0e5
Tastypotato245 <rbt2775@cau.ac.kr>
2025-01-01 19:26:47 +0900

    add: 15962

---
+++ 1/15962/main.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/15962
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+	cout << "파이팅!!";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
