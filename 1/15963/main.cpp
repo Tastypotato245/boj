70d3d9226e8d3b8b462ff974cf0de92901aaab9b
Tastypotato245 <rbt2775@cau.ac.kr>
2025-01-02 18:32:09 +0900

    add: 15963

---
+++ 1/15963/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/15963
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long N, M;
+
+void Solve()
+{
+	cin >> N >> M;
+	cout << (N == M);
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
