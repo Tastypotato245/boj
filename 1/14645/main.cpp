5f01f8ab2d335e0934ec55d3381a8029a949edf1
Tastypotato245 <rbt2775@cau.ac.kr>
2025-01-04 00:11:37 +0900

    add: 14645

---
+++ 1/14645/main.cpp
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/14645
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N, K;
+
+void Solve()
+{
+	cin >> N >> K;
+	int a, b;
+	for (int i = 0 ; i < N ; ++i)
+		cin >> a >> b;
+	cout << "비와이";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
