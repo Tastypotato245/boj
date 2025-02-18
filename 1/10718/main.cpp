3c2d7c63e3926fae7b4fa9f80e39317a02750012
Tastypotato245 <rbt2775@cau.ac.kr>
2025-02-18 20:37:32 +0900

    add: 10718

---
+++ 1/10718/main.cpp
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/10718
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+	cout << "강한친구 대한육군\n";
+	cout << "강한친구 대한육군\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
