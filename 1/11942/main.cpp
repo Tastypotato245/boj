cf9cc83b51ffa9d1129fe32b600ff9bec25e6bf1
Tastypotato245 <rbt2775@cau.ac.kr>
2025-02-18 20:38:29 +0900

    add: 11942

---
+++ 1/11942/main.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/11942
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+	cout << "고려대학교";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
