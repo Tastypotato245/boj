9f8cb93a4a12e25a18fd4cfd97e6f5d575e7cd9b
Tastypotato245 <rbt2775@cau.ac.kr>
2025-02-18 20:34:21 +0900

    add: 10189

---
+++ 1/10189/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/10189
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+	cout << "#  # #### #### #  #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#  # #### #### #  #\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
