bd00329232a571199322c6b13a7b6fe0c2f8c689
Tastypotato245 <rbt2775@cau.ac.kr>
2025-02-20 21:18:13 +0900

    add: 32384

---
+++ 3/32384/main.cpp
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/32384
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+
+	for (int i = 0 ; i < N ; ++i)
+		cout << "LoveisKoreaUniversity ";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
