2356df7ca8569a9bcbaae0cf5030801258c4e346
Tastypotato245 <rbt2775@cau.ac.kr>
2025-03-21 21:50:08 +0900

    add: 32951

---
+++ 3/32951/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/32951
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+	cout << N - 2024;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
