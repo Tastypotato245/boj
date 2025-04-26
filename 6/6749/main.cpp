b22f8ba67fbceaa9dbd5008d79d5145b0fce4df7
Tastypotato245 <rbt2775@cau.ac.kr>
2025-04-26 16:27:45 +0900

    add: 6749

---
+++ 6/6749/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/6749
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	A, B;
+
+void Solve()
+{
+	cin >> A >> B;
+	cout << B + B - A;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
