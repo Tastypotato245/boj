0fe8fb0c287c1a2959417f65cc736fd942000c10
Tastypotato245 <rbt2775@cau.ac.kr>
2025-05-26 20:40:57 +0900

    add: 13597

---
+++ 1/13597/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/13597
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	A, B;
+
+void Solve()
+{
+	cin >> A >> B;
+	cout << max(A, B);
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
