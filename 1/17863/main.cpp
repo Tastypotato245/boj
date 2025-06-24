05b215d19acd78671f4319946fb8d15a16162d82
miridih-kslee <kslee@miridih.com>
2025-06-24 19:42:37 +0900

    add: 17863

---
+++ 1/17863/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/17863
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+	cout << ((N / 10000 == 555) ? "YES" : "NO");
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
