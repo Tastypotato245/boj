e0453cec7f52a54afc3d4bae8ecbb60e150d474d
Tastypotato245 <rbt2775@cau.ac.kr>
2025-09-09 23:49:34 +0900

    add: 34297

---
+++ 3/34297/main.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/34297
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	M, K, C;
+
+void Solve()
+{
+	cin >> M >> K >> C;
+	cout << M * C;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
