efc23ff0ff669f74d0c018506503584334497b27
Tastypotato245 <rbt2775@cau.ac.kr>
2025-09-20 22:55:53 +0900

    add: 34411

---
+++ 3/34411/main.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/34411
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+    int m, n, y;
+    cin >> m >> n >> y;
+
+    int budget = m * n;
+    cout << budget;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
