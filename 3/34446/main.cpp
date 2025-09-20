ac511db4287cd1f91f6e25cc3774fc077ac2e19e
Tastypotato245 <rbt2775@cau.ac.kr>
2025-09-20 23:01:15 +0900

    add: 34446

---
+++ 3/34446/main.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/34446
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+    int m, n, t;
+    cin >> m >> n >> t;
+
+    int distance = 2 * m;
+    cout << distance;
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
