831b4504232f39b6b4107023b3c831310d884bce
Tastypotato245 <rbt2775@cau.ac.kr>
2025-09-27 21:50:54 +0900

    add: 26057

---
+++ 2/26057/main.cpp
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/26057
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	int l, t; cin >> l >> t;
+
+	int diff = 2 * t - l;
+	cout << diff << "\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
