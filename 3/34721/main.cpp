77a336d82b9463919ad75fe0a36dcd0a83f1f15e
Tastypotato245 <rbt2775@cau.ac.kr>
2025-11-09 23:55:57 +0900

    add: 34721

---
+++ 3/34721/main.cpp
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/34721
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int	N;
+
+void Solve()
+{
+	cin >> N;
+
+	for (int i = 0 ; i < N ; ++i)
+		cout << "I love DGU\n";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
