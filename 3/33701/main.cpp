d4fb0730c7539b1684df700cdc92a9c5fffdeb02
Tastypotato245 <rbt2775@cau.ac.kr>
2026-03-08 22:52:30 +0900

    add: 33701

---
+++ 3/33701/main.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/33701
+// github/Tastypotato245/boj
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+
+void Solve()
+{
+	cout << "nkugwan";
+}
+
+int	main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
